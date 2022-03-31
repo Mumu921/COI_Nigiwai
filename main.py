40be17d68b5815010f5a9814cdb1e079a27bcee4
Mumu921 <lin180921@gmail.com>
2022-03-31 21:33:01 +0900

    change

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    print("change 3")
+    print("change 5")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
